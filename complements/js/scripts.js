155c86c978cf984f486111b6b9f1ed5547a9302a
Hugo <hugodomconempresa@gmail.com>
2021-08-31 17:17:18 +0200

    Index from /ordenadores done!

--- complements/js/scripts.js
+++ complements/js/scripts.js
@@ -5,4 +5,17 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
         br = document.createElement("br")
         containers_br[index].appendChild(br)
     }
+    let h1_l = document.getElementsByClassName("h1-l")
+    for (let index = 0; index < h1_l.length; index++) {
+        h1_l[index].style.fontSize = "2.5em"
+    }
+    let clusters = document.getElementsByClassName("ccm")
+    for (let index = 0; index < clusters.length; index++) {
+        clusters[index].style.display = "block"
+        clusters[index].style.width = "80%"
+    }
+    let md = document.getElementsByClassName("md")
+    for (let index = 0; index < md.length; index++) {
+        md[index].remove()
+    }
 }
