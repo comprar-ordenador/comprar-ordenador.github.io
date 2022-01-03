8667043c8ea5daf2bed03de22c775dbc0c952b6a
Hugo <hugodomconempresa@gmail.com>
2022-01-03 13:30:12 +0100

    Restaured all important damages

--- SEO/change-images.py
+++ SEO/change-images.py
@@ -1,14 +1,20 @@
 from os import walk
 from os.path import join
 
+old_nav = '''
+
+'''
+new_nav = '''
+
+'''
+
 directory = input("Enter the directory:  ")
 for p, d, f in walk(directory):
     if ".git" not in p and 'imgs' not in p:
         for file in f:
             try:
                 text = open(join(p, file), 'r', encoding='utf-8').read()
-                text = text.replace("/ordenadores-para-negocios.webp", "/ordenadores-para-negocios.webp")
-                text = text.replace("/ordenadores-para-oficinas.webp", "/ordenadores-para-oficinas.webp")
+                text = text.replace(old_nav, new_nav)
                 open(join(p, file), 'w', encoding='utf-8').write(text)
             except:
                 pass
